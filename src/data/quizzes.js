0386588d7594d2b86228a81c078db00eafad6a2a
lewisolden <lewisolden5@gmail.com>
2025-01-28 12:39:19 +0000

    Create quizzes.js

---
+++ src/data/quizzes.js
@@ -0,0 +1,29 @@
+const quizzes = {
+  module1: [
+    {
+      id: 1,
+      question: "What is the fundamental problem Bitcoin was designed to solve?",
+      options: [
+        "Creating digital art",
+        "Solving the double-spending problem",
+        "Reducing bank fees",
+        "Creating online games"
+      ],
+      correctAnswer: 1
+    },
+    {
+      id: 2,
+      question: "What makes cryptocurrencies different from traditional currencies?",
+      options: [
+        "They are printed on special paper",
+        "They are controlled by a central bank",
+        "They are decentralized and use blockchain technology",
+        "They can only be used online"
+      ],
+      correctAnswer: 2
+    }
+  ],
+  // Additional module quizzes would be added here
+};
+
+export default quizzes;
