9bfb5a6e79df84aea2029861d6693e5edd89af04
lewisolden <lewisolden5@gmail.com>
2025-01-28 12:38:55 +0000

    Create courseModules.js

---
+++ src/data/courseModules.js
@@ -0,0 +1,34 @@
+const courseModules = [
+  {
+    id: "module1",
+    title: "Introduction to Cryptocurrency",
+    description: "Learn the fundamentals of digital currencies and blockchain technology",
+    chapters: [
+      "Overview of Cryptocurrency",
+      "History of Money",
+      "Bitcoin: The First Cryptocurrency"
+    ]
+  },
+  {
+    id: "module2",
+    title: "What is a Blockchain?",
+    description: "Deep dive into blockchain technology and its core principles",
+    chapters: [
+      "Blockchain Basics",
+      "Distributed Ledger Technology",
+      "Consensus Mechanisms"
+    ]
+  },
+  {
+    id: "module3",
+    title: "Blockchain Technology",
+    description: "Advanced exploration of blockchain systems and applications",
+    chapters: [
+      "Scalability and Interoperability",
+      "Types of Blockchains",
+      "Development Platforms"
+    ]
+  }
+];
+
+export default courseModules;
