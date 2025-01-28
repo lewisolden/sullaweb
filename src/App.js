23b3874eb1493bb23e9fcee9e76adf965323bef7
lewisolden <lewisolden5@gmail.com>
2025-01-28 16:44:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
 import CourseDetail from './pages/CourseDetail';
+import InteractiveLearning from './pages/InteractiveLearning';
+import Module1 from './pages/Module1';
+import Module2 from './pages/Module2';
+import Module3 from './pages/Module3';
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/courses/:moduleId" element={<CourseDetail />} />
+          <Route path="/interactive-learning" element={<InteractiveLearning />} />
+          <Route path="/module1" element={<Module1 />} />
+          <Route path="/module2" element={<Module2 />} />
+          <Route path="/module3" element={<Module3 />} />
         </Routes>
       </Layout>
     </Router>
