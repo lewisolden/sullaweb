1f5070c2905212649c626abfd0600c73879ea4fe
lewisolden <lewisolden5@gmail.com>
2025-01-28 20:14:32 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-// Filename: src/index.js
-// Purpose: Entry point of the React application
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ProgressProvider } from './contexts/ProgressContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProgressProvider>
+      <App />
+    </ProgressProvider>
   </React.StrictMode>
 );
