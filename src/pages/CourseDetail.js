51e7bf41d1237edd17cefc9ba88aacd5251db444
lewisolden <lewisolden5@gmail.com>
2025-01-28 13:39:27 +0000

    Update CourseDetail.js

--- src/pages/CourseDetail.js
+++ src/pages/CourseDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'; // Remove useEffect
 import { useParams } from 'react-router-dom';
 import courseModules from '../data/courseModules';
 
