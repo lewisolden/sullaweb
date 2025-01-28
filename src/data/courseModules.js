524df30f761af68e3706244db0535e8d9593ae6f
lewisolden <lewisolden5@gmail.com>
2025-01-28 18:55:28 +0000

    Create courseModules.js

---
+++ src/data/courseModules.js
@@ -0,0 +1,15 @@
+const courseModules = [
+  {
+    id: 1,
+    title: "Sample Module",
+    sections: [
+      {
+        id: 1.1,
+        title: "Sample Section",
+        content: "This is a sample content."
+      }
+    ]
+  }
+];
+
+export default courseModules;
