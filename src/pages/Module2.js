1a7aa3f5174777ebedeaa8204d9bdbd0972a33ae
lewisolden <lewisolden5@gmail.com>
2025-01-28 18:00:47 +0000

    Create Module2.js

---
+++ src/pages/Module2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Module2 = () => {
+  return (
+    <div>
+      <h1>Module 2: What is a blockchain?</h1>
+      <p>Content for Module 2 coming soon...</p>
+    </div>
+  );
+};
+
+export default Module2;
