e3eddc728a6002ff53e92d4f891f3e787441b633
lewisolden <lewisolden5@gmail.com>
2025-01-28 12:35:50 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import CourseDetail from './pages/CourseDetail';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:moduleId" element={<CourseDetail />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
