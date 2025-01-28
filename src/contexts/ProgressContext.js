882e3461524f5b30d01bae45db2d28b82a515d98
lewisolden <lewisolden5@gmail.com>
2025-01-28 22:42:25 +0000

    Update ProgressContext.js

--- src/contexts/ProgressContext.js
+++ src/contexts/ProgressContext.js
@@ -1,13 +1,10 @@
 src/
-│
 ├── pages/
 │   └── module1/
 │       ├── Module1Overview.js
 │       ├── DigitalCurrenciesSection.js
 │       ├── Module1Quiz.js
 │       └── index.js
-│
 ├── contexts/
 │   └── ProgressContext.js
-│
 └── App.js
