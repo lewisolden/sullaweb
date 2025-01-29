bdfa07c4b6a3333146f214a0814d79cc28b5cd96
lewisolden <lewisolden5@gmail.com>
2025-01-29 10:03:41 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 import App from './App';
 
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
